d133d5b601c63b9f63ae4addd4e821c63430e98d
Marcy <nadaso8th@gmail.com>
2024-06-06 12:50:05 -0500

    minor development on constructs

--- src/inerpreter/graph_gen/constructs/mod.rs
+++ src/inerpreter/graph_gen/constructs/mod.rs
@@ -4,7 +4,8 @@
 use super::rust::*;
 use crate::sim::circuit::operation::SignalID;
 
-/// Constructs a latch in module taking nodes set and reset as it's inputsl. The returned tuple is formatted as: (Q, !Q)
+/// Constructs a latch in module taking nodes set and reset as it's inputsl.
+/// The returned tuple is formatted as: (Q, !Q)
 fn latch(module: &mut RzModule, set: SignalID, rst: SignalID) -> (SignalID, SignalID) {
     let (q, q_not) = (module.rz_alloc(), module.rz_alloc());
     module.mk_nor(q, (rst,q_not)).unwrap();
@@ -20,34 +21,36 @@ fn flipflop(module: &mut RzModule, clk: SignalID, rst: SignalID, dat: SignalID, 
     }
 }
 
-fn register() {
+fn register(module: &mut RzModule, clk: Option<SignalID>, rst: SignalID, dat: &[SignalID], ce: Option<SignalID>) -> Box<[SignalID]> {
     unimplemented!("I make no guarantees as to if I will support clocked components")
 }
 
-fn complimenter() {
+fn compliment(module: &mut RzModule, dat: &[SignalID]) -> Box<[SignalID]> {
     unimplemented!("I make no guarantees as to if I will support interpreting of arithmatic operations")
 }
 
-fn full_adder() {
+/// Constructs a fulladder implementing x + y = z
+/// where the output tuple is formatted as (z, carry_out)
+fn full_adder(module: &mut RzModule, x:SignalID, y:SignalID, carry_in: SignalID) -> (SignalID, SignalID) {
     unimplemented!("I make no guarantees as to if I will support interpreting of arithmatic operations")
 }
 
-fn ripple_adder() {
+fn ripple_add(module: &mut RzModule) {
     unimplemented!("I make no guarantees as to if I will support interpreting of arithmatic operations")
 }
 
-fn multiplexer() {
+fn multiplex(module: &mut RzModule) {
     unimplemented!("I make no guarantees as to if I will support interpreting of non boolean logical operations")
 }
 
-fn variable_shift() {
+fn variable_shift(module: &mut RzModule) {
     unimplemented!("I make no guarantees as to if I will support interpreting of non boolean logical operations")
 }
 
-fn shift_multiplier() {
+fn shift_multiply(module: &mut RzModule) {
     unimplemented!("I make no guarantees as to if I will support interpreting of arithmatic operations")
 }
 
-fn shift_divider() {
+fn shift_divide(module: &mut RzModule) {
     unimplemented!("I make no guarantees as to if I will support interpreting of arithmatic operations")
 }

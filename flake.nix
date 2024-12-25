a5d3184a79c11f196f067f50e80c656e2181bc13
Ryan Butler <thebutlah@gmail.com>
2024-12-25 16:46:46 -0500

    support multiple systems via flake-utils

--- flake.nix
+++ flake.nix
@@ -3,53 +3,55 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    fenix.url = "github:nix-community/fenix";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs"; # avoids duplicating nixpkgs
+    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, fenix }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      rustToolchain = fenix.packages.${system}.fromToolchainFile {
-        file = ./rust-toolchain.toml;
-        sha256 = "sha256-s1RPtyvDGJaX/BisLT+ifVfuhDT1nZkZ1NcK8sbwELM=";
-      };
-      rustPlatform = pkgs.makeRustPlatform {
-        # inherit (rustToolchain) cargo rustc;
-        cargo = rustToolchain.cargo;
-        rustc = rustToolchain.rustc;
-      };
-      libPath = with pkgs; lib.makeLibraryPath [
-        libGL
-        libxkbcommon
-        wayland
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXrandr
-      ];
-    in
-    {
-
-      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = (with pkgs; [
-          xorg.libxcb
-          libxml2
-          llvm_19
-          llvmPackages_19.mlir
-        ]) ++ [
-          rustToolchain
-          rustPlatform.bindgenHook
+  outputs = { self, nixpkgs, fenix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        rustToolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-s1RPtyvDGJaX/BisLT+ifVfuhDT1nZkZ1NcK8sbwELM=";
+        };
+        rustPlatform = pkgs.makeRustPlatform {
+          # inherit (rustToolchain) cargo rustc;
+          cargo = rustToolchain.cargo;
+          rustc = rustToolchain.rustc;
+        };
+        libPath = with pkgs; lib.makeLibraryPath [
+          libGL
+          libxkbcommon
+          wayland
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
         ];
-        LD_LIBRARY_PATH = libPath;
-      };
+      in
+      {
 
-      formatter.${system} = pkgs.nixpkgs-fmt;
+        packages.hello = pkgs.hello;
+        packages.default = self.packages.hello;
 
-    };
+        devShells.default = pkgs.mkShell {
+          buildInputs = (with pkgs; [
+            xorg.libxcb
+            libxml2
+            llvm_19
+            llvmPackages_19.mlir
+          ]) ++ [
+            rustToolchain
+            rustPlatform.bindgenHook
+          ];
+          LD_LIBRARY_PATH = libPath;
+        };
 
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
 }

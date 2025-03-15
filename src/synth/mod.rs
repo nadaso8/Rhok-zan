629a473413ded8c63dbf15e1ec4aae16e585961e
nadaso8 <nadaso8th@gmail.com>
2025-03-15 11:26:31 -0500

    start work on new builder object

--- src/synth/mod.rs
+++ src/synth/mod.rs
@@ -1,2 +1,17 @@
 // mod AST;
 // mod SSA;
+
+use petgraph::graph::*;
+struct Netlist {
+    graph: Graph<dyn NetlistCell + Sized?, NetlistBus, >,
+}
+
+trait NetlistCell {
+    fn lower()
+}
+
+/// maps a source returned 
+struct NetlistBus {
+    source: String,
+    drain: String,
+}

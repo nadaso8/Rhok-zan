bbe3b29b3d338c5a49dc6bcb1ae5c5af090663a5
nadaso8 <nadaso8th@gmail.com>
2025-08-27 20:44:46 -0500

    cleanup main to remove references to now nonexistent frontend

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 mod back_end;
 mod front_end;
 mod middle_end;
-
-use front_end::gui_main;
-
 /// for now we just no questions asked run the gui
 /// eventualy I will make a proper cli and possibly
 /// tui interface
-fn main() {
-    gui_main().unwrap();
-}
+fn main() {}

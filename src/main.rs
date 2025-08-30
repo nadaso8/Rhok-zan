6de5b0b91f7c71f61b4ea7e98531598ffdaa9e19
nadaso8 <nadaso8th@gmail.com>
2025-08-30 17:53:58 -0500

    setup ratatui with simple example tutorial

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,9 @@ mod middle_end;
 /// for now we just no questions asked run the gui
 /// eventualy I will make a proper cli and possibly
 /// tui interface
-fn main() {}
+fn main() -> std::io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = front_end::Tui::default().run(&mut terminal);
+    ratatui::restore();
+    app_result
+}

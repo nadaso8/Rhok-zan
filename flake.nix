1d05310ea94457fc218b75a4809db8223ce96878
nadaso8 <nadaso8th@gmail.com>
2025-12-16 20:50:32 -0600

    update nixpkgs to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs"; # avoids duplicating nixpkgs
